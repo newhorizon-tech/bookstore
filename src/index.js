a2faa08176774a7bf82315f79bef1cd27837717f
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-11 23:44:35 +0530

    Add API for fetching books

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configStore';
+import { fetchBooks } from './redux/books/books';
+
+store.dispatch(fetchBooks);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,11 @@
 import { createSlice, current } from '@reduxjs/toolkit';
 
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+
+const apiId = 'a1XVlmDaV1nrjtVqGql8';
+
+const url = `${baseUrl}/apps/${apiId}/books`;
+
 const bookSlice = createSlice(
   {
     name: 'books',
@@ -7,9 +13,41 @@ const bookSlice = createSlice(
     reducers: {
       addBook: (state, action) => [...current(state), { ...action.payload }],
       removeBook: (state, action) => current(state).filter((b) => b.id !== action.payload),
+      fetchBooks: (state, action) => action.payload,
     },
   },
 );
+
+const saveBook = (book) => {
+  const saveBookThunk = async (dispatch) => {
+    const response = await fetch(url, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    });
+    const msg = await response;
+    console.log(msg);
+    dispatch({ type: 'books/addBook', payload: book });
+  };
+  return saveBookThunk;
+};
+
+const fetchBooks = async (dispatch) => {
+  const response = await fetch(url);
+  const books = await response.json();
+  const bookList = Object.entries(books).map(([id, [book]]) => (
+    {
+      item_id: id,
+      title: book.title,
+      category: book.category,
+    }
+  ));
+  dispatch({ type: 'books/fetchBooks', payload: bookList });
+};
+
+export { saveBook, fetchBooks };
 
 export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
