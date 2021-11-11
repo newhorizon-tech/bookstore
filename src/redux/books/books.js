15488253bb2c54f4ea4dc90a81b71f17b5623f53
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-11 19:54:14 +0530

    Create "removeBook" reducer using createSlice

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,19 @@
 import { createSlice, current } from '@reduxjs/toolkit';
 
-const sampleList = [{ id: 1, title: 'Great Circle', author: 'Maggie Shipstead' },
-  { id: 2, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-  { id: 3, title: 'Nothing', author: 'John Snow' }];
+const sampleList = [{ id: 'd0d7dcfb-5156-4134-aaac-b1b9451e3e7b', title: 'Great Circle', author: 'Maggie Shipstead' },
+  { id: 'bcf07ff4-bdf0-47a9-beca-56b35abbcd82', title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { id: '9fbf8521-abd0-46ee-a9fb-758d0151138e', title: 'Sample Book', author: 'John Sno' }];
 
 const bookSlice = createSlice(
   {
     name: 'books',
     initialState: sampleList,
     reducers: {
-      addBook: (state, action) => {
-        const newId = state.length + 1;
-        const newBook = { ...action.payload, id: newId };
-        const newState = [...current(state), newBook];
-        return newState;
-      },
+      addBook: (state, action) => [...current(state), { ...action.payload }],
+      removeBook: (state, action) => current(state).filter((b) => b.id !== action.payload),
     },
   },
 );
 
-export const { addBook } = bookSlice.actions;
+export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
