0956212d6d2e5a28c145e6ce14bd37b953669ba0
newhorizon-tech <alaukikinbox@gmail.com>
2021-11-12 23:46:30 +0530

    Fix form layout

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -24,11 +24,14 @@ const BooksPage = () => {
       <ul id="book-list">
         {bookList.map((book) => <Book key={book.item_id} book={book} />)}
       </ul>
-      <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
-        <input id="input-title" placeholder="Book Title" />
-        <input id="input-category" placeholder="Category" />
-        <button type="submit"> Add Book </button>
-      </form>
+      <div id="newbook">
+        <h3> ADD NEW BOOK </h3>
+        <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
+          <input id="input-title" placeholder="Book Title" />
+          <input id="input-category" placeholder="Category" />
+          <button type="submit"> ADD BOOK </button>
+        </form>
+      </div>
     </div>
   );
 };
