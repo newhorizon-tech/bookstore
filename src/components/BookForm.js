c1e181058610481a619d0a7c2a610895a1cecbc7
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-07 14:04:47 +0530

    Convert form into a controlled component

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,28 +1,26 @@
-import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { addBook } from '../redux/books/books';
 
 const BookForm = () => {
   const dispatch = useDispatch();
+  const [data, setData] = useState({});
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
 
   const submitForm = (e) => {
     e.preventDefault();
-    const title = e.target.querySelector('#input-title');
-    const author = e.target.querySelector('#input-author');
-    const data = {
-      id: uuidv4(),
-      title: title.value,
-      author: author.value,
-    };
-    title.value = '';
-    author.value = '';
     dispatch(addBook(data));
+    setData({ title: '', author: '' });
   };
+
   return (
     <form id="add-book-form" onSubmit={(e) => submitForm(e)}>
-      <input id="input-title" placeholder="Book Title" />
-      <input id="input-author" placeholder="Book Author" />
+      <input id="input-title" name="title" value={data.title} onChange={handleInput} placeholder="Book Title" />
+      <input id="input-author" name="author" value={data.author} onChange={handleInput} placeholder="Book Author" />
       <button type="submit"> Add Book </button>
     </form>
   );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,12 @@
 import { createSlice, current } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
 
 const bookSlice = createSlice(
   {
     name: 'books',
     initialState: [],
     reducers: {
-      addBook: (state, action) => [...current(state), { ...action.payload }],
+      addBook: (state, action) => [...current(state), { id: uuidv4(), ...action.payload }],
       removeBook: (state, action) => current(state).filter((b) => b.id !== action.payload),
     },
   },
