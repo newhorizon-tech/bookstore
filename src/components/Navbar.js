e5a2d9a9b5399cccf0f697f7b412af8e81beb055
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-10 21:19:43 +0530

    Add logo/favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,10 @@ import { GoPerson } from 'react-icons/go';
 
 const Navbar = () => (
   <nav id="navbar">
-    <h1 id="main-logo"> Biblioteca </h1>
+    <h1 id="main-logo">
+      <img src="/android-chrome-512x512.png" alt="logo" />
+      Biblioteca
+    </h1>
     <div className="nav-links">
       <Link to="/"> Books </Link>
       <Link to="/categories">Categories</Link>
